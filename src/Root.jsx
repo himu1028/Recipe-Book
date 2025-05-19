c754567df5cbc515f92cddd38464d2ccd55d491c
Abuzar hasan himu <abuzarhasanhimu0@gmail.com>
2025-05-19 21:57:23 +0600

    setup end

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 const Root = () => {
+    
     return (
-        <div>
+        <>
+            <Navbar></Navbar>
             <Outlet></Outlet>
-        </div>
+            <Footer></Footer>
+        </>
     );
 };
 
