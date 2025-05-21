1edeed985d60a53176ed69a8615f201889fe4cfd
Abuzar hasan himu <abuzarhasanhimu0@gmail.com>
2025-05-21 21:00:47 +0600

    review

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import TopRecipes from './TopRecipes';
 import HeroSlider from './HeroSlider';
 import Query from './Query';
+import Review from './Review';
 
 const Home = () => {
     return (
@@ -12,6 +13,9 @@ const Home = () => {
            <TopRecipes></TopRecipes> 
            <div className='w-11/12 bg-gray-300 px-5 py-10 mx-auto mt-3 rounded-lg'>
             <Query></Query>
+           </div>
+           <div className='w-11/12 bg-gray-300 px-5 py-10 mx-auto mt-3 rounded-lg'>
+            <Review></Review>
            </div>
         </>
     );
--- src/Review.jsx
+++ src/Review.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
 const Review = () => {
+
+    const handleReview = ()=>{
+alert("Thanks for Your Kind Suggestion")
+    }
     return (
-        <div>
-            
-        </div>
+        <>
+           <div >
+            <h2 className='text-4xl text-center font-bold text-purple-700'>Please Give Your Suggestion/Complain</h2>
+            <form className="fieldset text-center">
+  <legend className="fieldset-legend">Sueggestion or Complain</legend>
+  <textarea className="textarea w-[100%] h-40" placeholder="write please"></textarea>
+  <div className="label">Optional</div>
+  <button onClick={handleReview} className='btn btn-accent'>Submit</button>
+</form>
+            </div> 
+        </>
     );
 };
 
